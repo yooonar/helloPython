f6f28460f3751843ced10acaade8159fcb14f884
yoonar <aryooon33@gmail.com>
2019-12-13 14:13:42 +0900

    push! 2019-12-13

---
+++ Class/2.py
@@ -0,0 +1,15 @@
+def add (v1, v2) :
+    return v1 + v2
+
+def subtract(v1, v2) :
+    return v1 - v2
+
+num1 = 10
+num2 = 10
+print(add(num1, num2))
+print(subtract(num1, num2))
+
+num3 = 30
+num4 = 20
+print(add(num3, num4))
+print(subtract(num3, num4))
