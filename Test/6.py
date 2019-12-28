bf5af6428f9840e7b294b5d50cad562e2c777ed0
yooonar <aryooon33@gmail.com>
2019-12-28 23:59:55 +0900

    20191228 push!

--- Test/6.py
+++ Test/6.py
@@ -1,8 +1,9 @@
 # 문자열 다루기 기본
 def solution(s):
     return True if s.isdigit() == True and (len(s) == 4 or len(s) == 6) else False
-    # answer = False
-    # types = s.isdigit()
-    # if types == True and (len(s) == 4 or len(s) == 6) :
-    #     answer = True
-    # return answer
+# 다른 방법
+# answer = False
+# types = s.isdigit()
+# if types == True and (len(s) == 4 or len(s) == 6) :
+#     answer = True
+# return answer
