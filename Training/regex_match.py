bd133a4ff011651caac1fa3f782934a3737f99df
yooonar <aryooon33@gmail.com>
2020-12-01 00:51:50 +0900

    정규 표현식 - match 객체의 메소드

--- Training/regex_match.py
+++ Training/regex_match.py
@@ -10,3 +10,21 @@ print(result)
 nresult = p.match('3 python')
 print(nresult)
 # output: None
+
+# match 객체의 메소드 group, start, end, span
+
+# group: 매치된 문자열 리턴
+print(result.group())
+# output: python
+
+# start: 매치된 문자열의 시작 위치(인덱스) 리턴
+print(result.start())
+# output: 0
+
+# end: 매치된 문자열의 끝 위치(인덱스) 리턴
+print(result.end())
+# output: 6
+
+# span: 매치된 문자열의 (시작, 끝) 에 해당하는 튜플 리턴
+print(result.span())
+# output: (0, 6)
